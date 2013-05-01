1e18867668b822dd2d7effbc365463fc1477c7d5
Apprentice <apprentice@devbootcamp.com>
2013-05-01 15:04:52 -0700

    almost finished--needs html to submit comment

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+ //  $("#login").mouseenter(function(){
+ //    $(this).fadeTo('fast', 1);
+ //  });
+ //  $("#login").mouseleave(function(){
+ //    $(this).fadeTo('fast', 0.8);
+ //  });
+ // $("#signup").mouseenter(function(){
+ //    $(this).fadeTo('fast', 1); 
+ //  });
+ // $("#signup").mouseleave(function(){
+ //    $(this).fadeTo('fast', 0.8);
+ //  });
+ //  $("#submit").mouseenter(function(){
+ //    $(this).fadeTo('fast', 1); 
+ //  }); 
+ //  $("#submit").mouseleave(function(){
+ //    $(this).fadeTo('fast', 0.8);
+ //  });
 });
